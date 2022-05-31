f0f3256af058ac3d418274b1c2ac5c71e3566054
ShadowUser17 <linuxforce17@gmail.com>
2022-06-01 01:50:45 +0300

    Changes to be committed: 	modified:   router.go 	modified:   router_test.go

--- router.go
+++ router.go
@@ -1,6 +1,65 @@
 package httprouter
 
 import (
-	_ "net/http"
-	_ "net/http/httptrace"
+	"log"
+	"net/http"
+	"os"
+	"sync"
 )
+
+type HandlerFunc func(http.ResponseWriter, *http.Request, *log.Logger)
+
+type Endpoint map[string]HandlerFunc
+
+type Router struct {
+	mutex     *sync.Mutex
+	logger    *log.Logger
+	endpoints map[string]Endpoint
+}
+
+func NewRouter(logger *log.Logger) *Router {
+	if logger == nil {
+		logger = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	}
+
+	return &Router{
+		mutex:     new(sync.Mutex),
+		logger:    logger,
+		endpoints: make(map[string]Endpoint),
+	}
+}
+
+func (router *Router) GetHandler(location, method string) (HandlerFunc, int) {
+	router.mutex.Lock()
+	defer router.mutex.Unlock()
+
+	if endpoint, ok := router.endpoints[location]; ok {
+		if handler, ok := endpoint[method]; ok {
+			return handler, http.StatusOK
+
+		} else {
+			return nil, http.StatusMethodNotAllowed
+		}
+	}
+
+	return nil, http.StatusNotFound
+}
+
+func (router *Router) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	var handler, status = router.GetHandler(req.URL.EscapedPath(), req.Method)
+
+	if status == http.StatusOK {
+		handler(rw, req, router.logger)
+
+	} else {
+		rw.WriteHeader(status)
+	}
+}
+
+func (router *Router) HandleFunc(location, method string, handler HandlerFunc) {
+	if _, ok := router.endpoints[location]; !ok {
+		router.endpoints[location] = make(Endpoint)
+	}
+
+	router.endpoints[location][method] = handler
+}
--- router_test.go
+++ router_test.go
@@ -1,6 +1,51 @@
 package httprouter
 
 import (
-	_ "net/http/httptest"
-	_ "testing"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
+
+func handler(rw http.ResponseWriter, req *http.Request, out *log.Logger) {
+	rw.WriteHeader(http.StatusOK)
+	fmt.Fprintf(rw, "%s %s", req.Method, req.URL)
+}
+
+func TestRouter(t *testing.T) {
+	var mux = NewRouter(nil)
+	mux.HandleFunc("/", http.MethodHead, handler)
+	mux.HandleFunc("/", http.MethodGet, handler)
+	//mux.HandleFunc("/", http.MethodPost, handler)
+
+	var server = httptest.NewUnstartedServer(mux)
+	server.Start()
+
+	var client = server.Client()
+
+	if resp, err := client.Head(server.URL); err != nil {
+		t.Errorf("TestWebServer: %v\n", err)
+
+	} else {
+		t.Logf("TestWebServer: %s %s %s\n", resp.Request.Method, resp.Request.URL, resp.Status)
+		resp.Body.Close()
+	}
+
+	if resp, err := client.Get(server.URL); err != nil {
+		t.Errorf("TestWebServer: %v\n", err)
+
+	} else {
+		t.Logf("TestWebServer: %s %s %s\n", resp.Request.Method, resp.Request.URL, resp.Status)
+		resp.Body.Close()
+	}
+
+	if resp, err := client.Post(server.URL, "", strings.NewReader("POST...")); err != nil {
+		t.Errorf("TestWebServer: %v\n", err)
+
+	} else {
+		t.Logf("TestWebServer: %s %s %s\n", resp.Request.Method, resp.Request.URL, resp.Status)
+		resp.Body.Close()
+	}
+}
